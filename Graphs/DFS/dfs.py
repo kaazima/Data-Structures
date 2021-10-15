fd9a6a671dae5549a040395621d28971f609c6d7
Kaazima Ifrah <59416370+kaazima@users.noreply.github.com>
2021-10-15 13:47:54 +0530

    Graph dfs

---
+++ Graphs/DFS/dfs.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+# Function to return a list containing the DFS traversal of the undirected graph
+def dfs_graph(vertices, adj, path):
+    visited = set()
+    for node in range(vertices):
+        if node not in visited:
+            dfs(node, visited, path)
+    return path
+
+# Recursive function  
+def dfs(node, visited, path):
+    visited.add(node)
+    path.append(node)
+    for neighbour in adj[node]:
+        if neighbour not in visited:
+            dfs(neighbour, visited, path)
+
+print('---------------------------------------------------------------------')
+print('\tWelcome to Depth First Search for an undirected graph')
+print('---------------------------------------------------------------------\n')
+t = int(input("Enter the number of testcases: "))
+for _ in range(t):
+    print('\n*************** Testcase', _+1, '***************\n')
+    vertices, edges = map(int, input("Enter number of vertices & edges: ").split())
+    adj = [[] for vertex in range(vertices)]
+    for edge in range(edges):
+        start, end = map(int, input("Enter edge: ").split())
+        adj[start].append(end)
+        adj[end].append(start) # This line should be removed for directed graph
+    path = []
+    path = dfs_graph(vertices, adj, path)
+    print("\nDFS Traversal:")
+    for node in path:
+        print(node, end =' ')
+    print()
